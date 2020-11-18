e9da7820a55a19ef3f7656174e42ad38c221a531
Gianpaolo Macerola <gmacerola@gmail.com>
2020-11-18 16:29:54 -0600

    dsa-big-o assignment

---
+++ DSA-Big-O.js
@@ -0,0 +1,25 @@
+1. What is the Big O for this?
+    1 - O(1)
+    2 - O(n)
+
+2. Even or odd - O(1) constant, it doesn't matter what the value of the input is, it takes the same amount of time to calulate a modulo.
+
+3. Are you here? - O(n^k) polynomial time, O(n^2), 2 levels of looping.
+
+4. Doubler - O(n) linear, basically sums by doubling, time increases as value goes higher in a linear fashion.
+
+5. Naive search - O(n) linear, no method to the search, could be at the end of the array, gotta go through each in order.
+
+6. Creating pairs - O(n^k) polynomial time, O(n^2), 2 levels of looping.
+
+7. Compute the sequence - O(log(n)) logarithmic search,
+
+8. An efficient search - O(log(n)) logarithmic search, sorted array that splits it in half at first decreasing time.
+
+9. Random element - O(1) constant, same time no matter what the array is.
+
+10. What Am I - 
+
+11. Tower of Hanoi - See included towerOfHanoi.js file
+
+
---
+++ towerOfHanoi.js
@@ -0,0 +1,14 @@
+resolver_tower_of_hanoi = function (s, d, e, n) {
+  // terminating condition
+  if (n <= 0) {
+    return;
+  }
+
+  resolver_tower_of_hanoi(s, e, d, n - 1);
+
+  console.log(`Move Disk-${n} FROM ${s} TO ${d}`);
+
+  resolver_tower_of_hanoi(e, d, s, n - 1);
+};
+
+resolver_tower_of_hanoi("s", "d", "e", 5);
